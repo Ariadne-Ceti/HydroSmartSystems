bb7db32e6e5cfde34183fe24d1ee4bda0fb8be5a
Ariadne-Ceti <a22300044@ceti.mx>
2026-06-03 13:44:58 -0600

    Primera versión del sitio

---
+++ scrip.js
@@ -0,0 +1,12 @@
+function login(){
+document.getElementById("mensajeAuth").innerText="Inicio de sesión exitoso (simulado)";
+}
+
+function register(){
+document.getElementById("mensajeAuth").innerText="Usuario registrado correctamente (simulado)";
+}
+
+function enviarEncuesta(e){
+e.preventDefault();
+document.getElementById("respuestaEncuesta").innerText="Gracias por tu opinión 💧";
+}
